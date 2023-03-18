2fa8315d91c47fe0c90d142a7aac8ea91d082760
KarenRinconChrininos <krinconc1400@alumno.ipn.mx>
2023-03-18 10:00:56 -0600

    update

--- arbolbinariodebusqueda/src/EjemploArbolBinario.java
+++ arbolbinariodebusqueda/src/EjemploArbolBinario.java
@@ -7,29 +7,36 @@ public class EjemploArbolBinario {
         int izquierda = 0;
         int derecha = 0;
 
+        if(izquierda > 0 ){
+            System.out.println("s");
+        }else{
+            System.out.println("aaaa");
+        }
+
         while(cont < num.length){
             padre = num[cont];
-            c = 1;
             System.out.println(padre); //
             if(num[cont] !=  0){
+                if(cont+2 != 0){
+                    c = 1;
+                }else{
+                    c = 2;
+                }
                 while( c <= cont+2){
-                    if(padre < num[cont+c]) {
+                    if(num[cont+c] < padre) {
                         //menor - izquierda
                         izquierda = num[cont+c];
                     }else{
                         //mayor - derecha
                         derecha = num[cont+c];
                     }
-                    if(num[cont+c] > num[cont+3]){
-                        padre = num[cont+c];
-                    }
                     c++;
                 }
+                cont+=c;
                 System.out.println(izquierda + "  " + derecha);
             }else{
                break;
             }
-            cont++;
         }
     }
 }
